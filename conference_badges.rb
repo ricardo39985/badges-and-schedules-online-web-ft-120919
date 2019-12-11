938feac06b4a35158c285af602493e52b56aa737
Ricardo Persaud <ricardo39985@gmail.com>
2019-12-11 23:26:25 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,6 +4,6 @@ def badge_maker(name)
 end
 def batch_badge_creator(names)
   badge_messages=[]
-  names.each_with_index{|name, room_plus| badge_messages.push("Hello, #{name}! You'll be assigned to room #{room_plus + 1}!")}
+  names.each{|name| badge_messages.push("Hello, my name is #{name}."}
   badge_messages
 end
